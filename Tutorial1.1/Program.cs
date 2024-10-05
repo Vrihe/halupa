cf08faed5f570e12f804a5c888def3ea616c3aba
ddnzbtw <spaceomon@gmail.com>
2024-10-05 13:58:11 +0200

    Adding tutorial

---
+++ Tutorial1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+
+
+//условная папка с кодом
+
+namespace Tutorial1._1
+{
+    class CTutorial
+    {  //Точка входа в прогу
+        static void Main()
+        {  //Исполняемый код
+            Console.WriteLine("el niggas la capuero");
+            //Ожидание ввода символа пользователем
+            Console.ReadKey();
+        }
+
+    }
+
+}
