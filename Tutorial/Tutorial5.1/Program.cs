e46526451c416f26a21c1972300d301117cb1e9e
ddnzbtw <spaceomon@gmail.com>
2024-10-06 19:03:58 +0200

    cycles

--- Tutorial/Tutorial5.1/Program.cs
+++ Tutorial/Tutorial5.1/Program.cs
@@ -4,7 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("введите значение");
+            float fNumber = float.Parse(Console.ReadLine());
+
+            а цикл for хуйня
+
+
+            for (; fNumber > 5.0f; fNumber--)
+                {
+                    Console.WriteLine("Значение {0}", fNumber);
+
+                    bool bPidr = true;
+
+                    //переменная bool всегда должна быть со значением true 
+                    //чтобы цикл выполнялся
+
+                    //цикл будет идти до тех пор пока не значение не достигнет требуемого
+
+                    while (bPidr)
+                    {
+
+
+                        Console.WriteLine("ты пидор?");
+
+                        string sEnd = Console.ReadLine();
+
+                        if (sEnd == "ладно я пидор")
+                        {
+                            bPidr = false;
+
+                        }
+
+
+                        //Цикл будет идти до тех пор пока значение while будет неверным
+                        do
+                        {
+                            Console.WriteLine("Значение переменной: " + fNumber);
+                            fNumber--;
+                        } while (fNumber <= 4 && fNumber >= -4);
+
+                        //оператор break может завершать циклы
+                        //только писать его нужно через if
+
+                        //continue пропускает 1 круг цикла при выполнении условий if
+                    }
+                }
         }
-    }
-}
