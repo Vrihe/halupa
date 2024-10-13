56eb69cd675e250b0d3f3bee2a5475fb90caa058
ddnbztw <Spaceomon@gmail.com>
2024-10-13 17:43:30 +0200

    da

---
+++ Tutorial/Tutorial8.1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Tutorial8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           cRobot bot_0 = new cRobot();
+            bot_0.sName = "Dolboeb";
+            bot_0.iWeight = 300;
+            bot_0.aCoordinates = new byte[] { 0, 0, 0, };
+
+
+            cRobot cBot_1 = new cRobot();
+
+                cBot_1.sName = "Debil";
+                cBot_1.iWeight = 1300;
+                cBot_1.aCoordinates = new byte[] { 0, 0, 10,};
+
+            cRobobot cBobot_0 = new cRobobot();
+            cBobot_0.setValues("Dildo", 990, new byte[] {0,0,0}) ;
+            cBobot_0.printValues();          
+
+
+
+        }
+    }
+}
---
+++ Tutorial/Tutorial8.1/Robot.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial8._1
+{
+    class cRobot
+    {
+        public string sName;
+
+        public int iWeight;
+
+        public byte[] aCoordinates;
+    }
+
+
+    class cRobobot
+    {
+
+        private string sName;
+
+        private int iWeight;
+
+        private byte[] aCoordinates;
+
+        public void setValues(string name, int weight, byte[] coordinates)
+        {
+            sName = name;
+            iWeight = weight;
+            aCoordinates = coordinates;
+        }
+
+        public void printValues()
+        {
+            Console.WriteLine(sName + "weight: " + iWeight + ".Coordinates: ");
+            foreach (byte el in aCoordinates)
+            {
+                Console.WriteLine(el);
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
